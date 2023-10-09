19fcf0e63a61343f7a3b6ddfb64d5e67ad830505
ShakilFG22 <w82l8.islam@gmail.com>
2023-10-09 13:43:53 +0200

    New class

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,1 +1,11 @@
-com.unity.netcode
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct Spawner : IComponentData
+{
+    public Entity Prefab;
+    public float3 SpawnPosition;
+    public float NextSpawnTime;
+    public float SpawnRate;
+}
+
