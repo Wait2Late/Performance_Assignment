5ee6d8cb932c0e4036fbb3b8ca669dc2e2259563
ShakilFG22 <w82l8.islam@gmail.com>
2023-10-12 08:57:51 +0200

    Commented out some code that disrupted the whole scenes even though it was not a part of the game no longer
    Commented some extra trivia on PlayerSpawnerSystem

---
+++ Assets/Scripts/Player/PlayerSpawnerSystem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public partial class PlayerSpawnerSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        // EntityQuery playerEntityQuery = EntityManager.CreateEntityQuery(typeof(PlayerTag)); //This one as well
+        // PlayerSpawnerComponent playerSpawnerComponent = SystemAPI.GetSingleton<PlayerSpawnerComponent>(); // For some reason this need to assign to something otherwise errors?
+
+        EntityCommandBuffer entityCommandBuffer = 
+            SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(World.Unmanaged);
+        
+        
+        int spawnAmount = 2;
+        // if (playerEntityQuery.CalculateEntityCount() < spawnAmount)
+        // {
+        //     // /* Entity spawnedEntity =*/ entityCommandBuffer.Instantiate(playerSpawnerComponent.playerPrefab);
+        //     // entityCommandBuffer.SetComponent();
+        //     // EntityManager.Instantiate(playerSpawnerComponent.playerPrefab);
+        // }
+
+    }
+}
